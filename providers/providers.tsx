9740f0e7cc6f84f8d625b5662c51e0d0794084c1
kushbosamiya <kushalbosamiya13@gamil.com>
2026-03-20 17:56:28 +0530

    chore: format codebase with prettier

--- providers/providers.tsx
+++ providers/providers.tsx
@@ -11,9 +11,7 @@ const queryClient = new QueryClient();
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        {children}
-      </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </WagmiProvider>
   );
 }

d01a860cd0b8970c68872b550841515c3fae45fe
kushbosamiya <kushalbosamiya13@gamil.com>
2026-03-22 19:00:13 +0530

    fix: add missing FiMenu import to resolve build failure

--- components/layout/SideBar.tsx
+++ components/layout/SideBar.tsx
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { FiMenu } from "react-icons/fi";
 
 interface NavItem {
   label: string;
